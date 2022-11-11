1723295a00de31c65b56c948085b071ffc2e1c9b
Alihub20 <116361824+Alihub20@users.noreply.github.com>
2022-11-12 00:32:48 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo mv /home/box/Stepic1/web ~
+sudo mv /home/box/Stepic20/web ~
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
